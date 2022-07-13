96b2a9008b6eebadb36677619c513a758392e266
neo@zillinks.com <keunhey0107@gmail.com>
2022-07-13 11:16:18 +0900

    중복 코드 제거 / 몽고디비 코드 상위로 옮김

--- login.py
+++ login.py
@@ -5,6 +5,8 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ziyis.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+client = MongoClient('mongodb+srv://test:sparta@cluster0.kx1zt.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -20,7 +22,10 @@ import datetime
 # 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
 import hashlib
 
+import requests
+from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
 #################################
 ##  HTML을 주는 부분             ##
 #################################
@@ -46,6 +51,11 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/index')
+def page():
+    return render_template('index.html')
+
 
 
 #################################
@@ -128,81 +138,61 @@ def api_valid():
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?orderClick=d79', headers=headers)
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000, debug=True)
+soup = BeautifulSoup(data.text, 'html.parser')
+books = soup.select('#main_contents > ul > li')
+for book in books[:20]:
+    title = book.select_one('div.detail > div.title > a > strong').text
+    img = book.select_one('div.cover > a > img')['src']
+    rank = book.select_one('div.cover > a > strong').text
+    done = 0
+    bid = book.select_one('div.detail > div.title > a')['href'].split('barcode=')[1]
 
-    from flask import Flask, render_template, request, jsonify
+    doc = {
+        'title': title, 'img': img, 'rank': rank, 'done': done,
+    }
+    db.books.insert_one(doc)
 
-    app = Flask(__name__)
 
-    import requests
-    from bs4 import BeautifulSoup
+# all_users = list(db.users.find({},{'_id':False}))
 
-    from pymongo import MongoClient
+# main_contents > ul > li:nth-child(6) > div.detail > div.subtitle
 
-    client = MongoClient('mongodb+srv://test:sparta@cluster0.kx1zt.mongodb.net/Cluster0?retryWrites=true&w=majority')
-    db = client.dbsparta
+
+
+@app.route("/books", methods=["POST"])
+def book_post():
+    url_receive = request.form['url_give']
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?orderClick=d79', headers=headers)
+    data = requests.get(url_receive, headers=headers)
 
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    books = soup.select('#main_contents > ul > li')
-    for book in books[:20]:
-        title = book.select_one('div.detail > div.title > a > strong').text
-        img = book.select_one('div.cover > a > img')['src']
-        rank = book.select_one('div.cover > a > strong').text
-        done = 0
-        bid = book.select_one('div.detail > div.title > a')['href'].split('barcode=')[1]
-
-        doc = {
-            'title': title, 'img': img, 'rank': rank, 'done': done,
-        }
-        db.books.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
 
 
-    # all_users = list(db.users.find({},{'_id':False}))
-
-    # main_contents > ul > li:nth-child(6) > div.detail > div.subtitle
-    @app.route('/index')
-    def home():
-        all_books = list(db.books.find({}, {'_id': False}))
-        return render_template('index.html', bundle=all_books)
+@app.route("/books", methods=["GET"])
+def movie_get():
+    book_list = list(db.books.find({}, {'_id': False}))
+    return jsonify({'books': book_list})
 
 
-    @app.route("/books", methods=["POST"])
-    def book_post():
-        url_receive = request.form['url_give']
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-        data = requests.get(url_receive, headers=headers)
-
-        return jsonify({'msg': '저장 완료!'})
+@app.route("/books/done", methods=["POST"])
+def rank_done():
+    rank_receive = request.form['rank_give']
+    db.books.update_one({'rank': rank_receive}, {'$set': {'done': 1}})
+    return jsonify({'msg': '등록 완료!'})
 
 
-    @app.route("/books", methods=["GET"])
-    def movie_get():
-        book_list = list(db.books.find({}, {'_id': False}))
-        return jsonify({'books': book_list})
+@app.route("/books/cancel", methods=["POST"])
+def cancel_done():
+    rank_receive = request.form['rank_give']
+    db.books.update_one({'rank': rank_receive}, {'$set': {'done': 0}})
+    return jsonify({'msg': '취소 완료!'})
 
 
-    @app.route("/books/done", methods=["POST"])
-    def rank_done():
-        rank_receive = request.form['rank_give']
-        db.books.update_one({'rank': rank_receive}, {'$set': {'done': 1}})
-        return jsonify({'msg': '등록 완료!'})
-
-
-    @app.route("/books/cancel", methods=["POST"])
-    def cancel_done():
-        rank_receive = request.form['rank_give']
-        db.books.update_one({'rank': rank_receive}, {'$set': {'done': 0}})
-        return jsonify({'msg': '취소 완료!'})
-
-
-    if __name__ == '__main__':
-        app.run('0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
