01bc2b5dc4c669b664a4c7117e380f7a80cd1024
tkrkr55 <108445626+tkrkr55@users.noreply.github.com>
2022-07-14 09:06:00 +0900

    mypage 수정

--- app.py
+++ app.py
@@ -13,3 +13,16 @@ client = MongoClient('mongodb+srv://test:sparta@cluster0.mt2g7.mongodb.net/Clust
 db = client.dbsparta
 
 
+@app.route('/')
+def mypage():
+    return render_template('mypage.html')
+
+
+@app.route("/books", methods=["GET"])
+def dones():
+    book_list = list(db.books.find({}, {'_id': False}))
+    return jsonify({'books': book_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
